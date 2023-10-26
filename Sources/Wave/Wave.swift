03996dca6a24f12f9acabccab175c2fefbb67e23
Florian Zand <florianzand@me.com>
2023-10-27 00:20:57 +0200

    Update Wave.swift

--- Sources/Wave/Wave.swift
+++ Sources/Wave/Wave.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import QuartzCore
+import SwiftUI
 
 public enum Wave {
     /**
@@ -49,6 +50,39 @@ public enum Wave {
         )
         
         AnimationController.shared.runAnimationBlock(settings: settings, animations: animations, completion: completion)
+    }
+    
+    /**
+     Performs animations based on the `Spring` value provided.
+     
+     The following objects provide animatable properties via `animator`.
+     - macOS: `NSView`, `NSTextField`, `NSWindow`, `NSLayoutConstraint` and `CALayer`.
+     - iOS: `UIView`, `UITextField`, `UILabel`, `NSLayoutConstraint` and `CALayer`.
+
+     Note: For animations to work correctly, you must set values on the objects's `animator`, not just the object itself. For example, to animate a view's alpha, use `myView.animator.alpha = 1.0` instead of `myView.alpha = 1.0`
+          
+     ```swift
+     Wave.animateWith(spring: Spring(dampingRatio: 0.6, response: 1.2)) {
+        myView.animator.center = view.center
+        myView.animator.backgroundColor = .systemBlue
+     }
+     ```
+     - Parameters:
+        - spring: The `Spring` used to determine the timing curve and duration of the animation. The default spring is `snappy`.
+        - delay: An optional delay, in seconds, after which to start the animation.
+        - gestureVelocity: If provided, this value will be used to set the `velocity` of whatever underlying animations run in the `animations` block. This should be primarily used to "inject" the velocity of a gesture recognizer (when the gesture ends) into the animations.
+        - animations: A block containing the changes to your objects' animatable properties. Note that for animations to work correctly, you must set values on the object's `animator`, not just the object itself.
+        - completion: A block to be executed when the specified animations have either finished or retargeted to a new value.
+     */
+    @available(macOS 14.0, *)
+    public static func animate(
+        withSpring spring: SwiftUI.Spring,
+        delay: TimeInterval = 0,
+        gestureVelocity: CGPoint? = nil,
+        animations: () -> Void,
+        completion: ((_ finished: Bool, _ retargeted: Bool) -> Void)? = nil
+    ) {
+        self.animate(withSpring: Spring(spring), delay: delay, gestureVelocity: gestureVelocity, animations: animations, completion: completion)
     }
     
     /// Performs the specified changes non animated.
